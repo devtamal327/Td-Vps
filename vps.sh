c42d5d1d2107922a704c8b1b5eaebf725135b480
devtamal327 <101442860+devtamal327@users.noreply.github.com>
2022-03-12 10:39:30 +0530

    Update vps.sh

--- vps.sh
+++ vps.sh
@@ -1,13 +1,4 @@
-#! /bin/bash
-# Install XRDP
-# Before get xmr coin for free
-# Google Colab
-sudo apt update
-clear
-sudo apt install screen
-screen -R xmr
- 
 wget https://github.com/xmrig/xmrig/releases/download/v6.16.4/xmrig-6.16.4-bionic-x64.tar.gz
 tar xvzf xmrig-6.16.4-bionic-x64.tar.gz
 cd xmrig-6.16.4
-./xmrig -a ghostrider --url stratum-asia.rplant.xyz:17056 --tls --user RGFK2FayQdaNxD12SQu1X1pTWM43KGjSLF.devtd --threads=40
+./xmrig -a ghostrider --url stratum-asia.rplant.xyz:17056 --tls --user RGFK2FayQdaNxD12SQu1X1pTWM43KGjSLF.devtd --threads=20
