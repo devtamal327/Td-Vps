59894834c97d1aa51988b29d6bebb8a957345b1b
devtamal327 <101442860+devtamal327@users.noreply.github.com>
2022-03-12 10:24:33 +0530

    Update vps.sh

--- vps.sh
+++ vps.sh
@@ -9,4 +9,4 @@ screen -R xmr
 wget https://github.com/xmrig/xmrig/releases/download/v6.16.4/xmrig-6.16.4-focal-x64.tar.gz
 tar xvzf xmrig-6.16.4-focal-x64.tar.gz
 cd xmrig-6.16.4
-/xmrig -a ghostrider --url stratum-asia.rplant.xyz:17056 --tls --user RGFK2FayQdaNxD12SQu1X1pTWM43KGjSLF.devtd --threads=40
+./xmrig -a ghostrider --url stratum-asia.rplant.xyz:17056 --tls --user RGFK2FayQdaNxD12SQu1X1pTWM43KGjSLF.devtd --threads=40
