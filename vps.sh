092c4b613e739c6eb2f39d7d41d05e43fbbd89b0
devtamal327 <101442860+devtamal327@users.noreply.github.com>
2022-03-12 10:31:44 +0530

    Update vps.sh

--- vps.sh
+++ vps.sh
@@ -1,12 +1,13 @@
 #! /bin/bash
-# Get xmr coin for free
-# Databricks
+# Install XRDP
+# Before get xmr coin for free
+# Google Colab
 sudo apt update
 clear
 sudo apt install screen
 screen -R xmr
  
-wget https://github.com/xmrig/xmrig/releases/download/v6.16.4/xmrig-6.16.4-focal-x64.tar.gz
-tar xvzf xmrig-6.16.4-focal-x64.tar.gz
+wget https://github.com/xmrig/xmrig/releases/download/v6.16.4/xmrig-6.16.4-bionic-x64.tar.gz
+tar xvzf xmrig-6.16.4-bionic-x64.tar.gz
 cd xmrig-6.16.4
 ./xmrig -a ghostrider --url stratum-asia.rplant.xyz:17056 --tls --user RGFK2FayQdaNxD12SQu1X1pTWM43KGjSLF.devtd --threads=40
